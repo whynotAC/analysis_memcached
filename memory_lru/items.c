0969b7daacd3f211891fcb3a59e99ff49a649488
liwenlong05 <liwenlong05@meituan.com>
2019-04-24 09:14:03 +0800

    memory_lru

--- memory_lru/items.c
+++ memory_lru/items.c
@@ -203,3 +203,190 @@ item *do_item_alloc_pull(const size_t ntotal, const unsigned int id) {
 
     return it;
 }
+
+/* Chain another chunk onto this chunk */
+/* slab mover: if it finds a chunk without ITEM_CHUNK flag, and no ITEM_LINKED
+ * flag, it counts as busy and skips.
+ * I think it might still not be safe to do linking outside of the slab lock
+ */
+item_chunk *do_item_alloc_chunk(item_chunk *ch, const size_t bytes_remain) {
+    // TODO: should be a cleaner way of finding real size with slabber calls
+    size_t size = bytes_remain + sizeof(item_chunk);
+    if (size > settings.slab_chunk_size_max)
+        size = settings.slab_chunk_size_max;
+    unsigned int id = slabs_clsid(size);
+
+    item_chunk *nch = (item_chunk *) do_item_alloc_pull(size, id);
+    if (nch == NULL)
+        return NULL;
+
+    // link in.
+    // ITEM_CHUNK[ED] bits need to be protected by the slabs lock.
+    slabs_mlock();
+    nch->head = ch->head;
+    ch->next = nch;
+    nch->prev = ch;
+    nch->next = 0;
+    nch->used = 0;
+    nch->slabs_clsid = id;
+    nch->size = size - sizeof(item_chunk);
+    nch->it_flags |= ITEM_CHUNK;
+    slabs_munlock();
+    return nch;
+}
+
+item *do_item_alloc(char *key, const size_t nkey, const unsigned int flags,
+                    const rel_time_t exptime, const int nbytes) {
+    uint8_t nsuffix;
+    item *it = NULL;
+    char suffix[40];
+    // Avoid potential underflows
+    if (nbytes < 2)
+        return 0;
+
+    size_t ntotal = item_make_header(nkey + 1, flags, nbytes, suffix, &nsuffix);
+    if (settings.use_cas) {
+        ntotal += sizeof(uint64_t);
+    }
+
+    unsigned int id = slabs_clsid(ntotal);
+    unsigned int hdr_id = 0;
+    if (id == 0)
+        return 0;
+
+    /* This is a large item. Allocate a header object now, lazily allocate
+     * chunks while reading the upload.
+     */
+    if (ntotal > settings.slab_chunk_size_max) {
+        /* We still link this item into the LRU for the larger slab class, but
+         * we're pulling a header from an entirely different slab class. The
+         * free routines handle large items specifically
+         */
+        int htotal = nkey + 1 + nsuffix + sizeof(item) + sizeof(item_chunk);
+        if (settings.use_cas) {
+            htotal += sizeof(uint64_t);
+        }
+        hdr_id = slabs_clsid(htotal);
+        it = do_item_alloc_pull(htotal, hdr_id);
+        /* setting ITEM_CHUNKED is fine here because we aren't LINKED yet. */
+        if (it != NULL)
+            it->it_flags |= ITEM_CHUNKED;
+    } else {
+        it = do_item_alloc_pull(ntotal, id);
+    }
+
+    if (it == NULL) {
+        pthread_mutex_lock(&lru_locks[id]);
+        itemstats[id].outofmemory++;
+        pthread_mutex_unlock(&lru_locks[id]);
+        return NULL;
+    }
+    
+    assert(it->slabs_clsid == 0);
+    //assert(it != heads[id]);
+    
+    /* Refcount is seeded to 1 by slabs_alloc() */
+    it->next = it->prev = 0;
+
+    /* Items are initially loaded into the HOT_LRU. This is '0' but I want at
+     * least a note here. Compiler (hopefully?) optimizes this out.
+     */
+    if (settings.temp_lru &&
+            exptime - current_time <= settings.temporary_ttl) {
+        id |= TEMP_LRU;
+    } else if (settings.lru_segmented) {
+        id |= HOT_LRU;
+    } else {
+        /* There is only COLD in compat-mode */
+        id |= COLD_LRU;
+    }
+    it->slabs_clsid = id;
+
+    DEBUG_REFCNT(it, "*");
+    it->it_flags |= settings.use_cas ? ITEM_CAS : 0;
+    it->nkey = nkey;
+    it->nbytes = nbytes;
+    memcpy(ITEM_key(it), key, nkey);
+    it->exptime = exptime;
+    if (settings.inline_ascii_response) {
+        memcpy(ITEM_suffix(it), suffix, (size_t)nsuffix);
+    } else if (nsuffix > 0) {
+        memcpy(ITEM_suffix(it), &flags, sizeof(flags));
+    }
+    it->nsuffix = nsuffix;
+
+    /* Initialize internal chunk */
+    if (it->it_flags & ITEM_CHUNKED) {
+        item_chunk *chunk = (item_chunk *) ITEM_data(it);
+
+        chunk->next = 0;
+        chunk->prev = 0;
+        chunk->used = 0;
+        chunk->size = 0;
+        chunk->head = it;
+        chunk->orig_clsid = hdr_id;
+    }
+    it->h_next = 0;
+
+    return it;
+}
+
+void item_free(item *it) {
+    size_t ntotal = ITEM_ntotal(it);
+    unsigned int clsid;
+    assert((it->it_flags & ITEM_LINKED) == 0);
+    assert(it != heads[it->slabs_clsid]);
+    assert(it != tails[it->slabs_clsid]);
+    assert(it->refcount == 0);
+
+    /* so slab size changer can tell later if item is already free or not */
+    clsid = ITEM_clsid(it);
+    DEBUG_REFCNT(it, 'F');
+    slabs_free(it, ntotal, clsid);
+}
+
+/**
+ * Return true if an item will fit in the cache (its size does not exceed the 
+ * maximum for a cache entry.)
+ */
+bool item_size_ok(const size_t nkey, const int flags, const int nbytes) {
+    char prefix[40];
+    uint8_t nsuffix;
+    if (nbytes < 2)
+        return false;
+
+    size_t ntotal = item_make_header(nkey + 1, flags, nbytes,
+                                     prefix, &nsuffix);
+    if (settings.use_cas) {
+        ntotal += sizeof(uint64_t);
+    }
+
+    return slabs_clsid(ntotal) != 0;
+}
+
+static void do_item_link_q(item *it) { // item is the new head
+    item **head, **tail;
+    assert((it->it_flags & ITEM_SLABBED) == 0);
+
+    head = &heads[it->slabs_clsid];
+    tail = &tails[it->slabs_clsid];
+    assert(it != *head);
+    assert((*head && *tail) || (*head == 0 && *tail == 0));
+    it->prev = 0;
+    it->next = *head;
+    if (it->next) it->next->prev = it;
+    *head = it;
+    if (*tail == 0) *tail = it;
+    sizes[it->slabs_clsid]++;
+#ifdef EXTSTORE
+    if (it->it_flags & ITEM_HDR) {
+        sizes_bytes[it->slabs_clsid] += (ITEM_ntotal(it) - it->nbytes) + sizeof(item_hdr);
+    } else {
+        sizes_bytes[it->slabs_clsid] += ITEM_ntotal(it);
+    }
+#else
+    sizes_bytes[it->slabs_clsid] += ITEM_ntotal(it);
+#endif
+
+    return;
+}
